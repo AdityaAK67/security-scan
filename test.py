4ec3094bef54c92b438ce5afc57243313bd0a1bd
AdityaAK67 <adikandalkar838@gmail.com>
2025-12-30 00:15:45 +0530

    test: add hardcoded secret

--- test.py
+++ test.py
@@ -27,6 +27,7 @@ def get_env_variable():
     return os.environ.get("SECRET_KEY", "")
 API_KEY = "sk_test_1234567890abcdef"
 
+
 def execute_shell(cmd):
     # Avoid shell=True, split command into list and prevent command injection by allowing only safe commands
     allowed_commands = {"ls", "whoami"}
